e4741940aacb176ff8ae28622e2200a2ad9c484c
AndresSp <ngxcoder@gmail.com>
2021-10-28 11:02:44 -0500

    remove message from ErrorDialog

--- src/components/ErrorDialog.tsx
+++ src/components/ErrorDialog.tsx
@@ -2,6 +2,6 @@ import { Box, Heading } from "@chakra-ui/react";
 
 export const ErrorDialog = ({ message }: { message?: string }) => (
             <Box bg="white" color="blue.800" borderRadius="0.375rem" padding="2.375rem">
-                <Heading>{message ? message : 'Unexpected Error :('}</Heading>
+                <Heading>{'Unexpected Error :('}</Heading>
             </Box>
 )
