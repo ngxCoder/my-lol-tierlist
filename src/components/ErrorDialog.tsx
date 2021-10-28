417a0861b07c6ebb37cdfa3167524a3afb8500e8
AndresSp <ngxcoder@gmail.com>
2021-10-28 11:11:54 -0500

    Truncate to 50 error message

--- src/components/ErrorDialog.tsx
+++ src/components/ErrorDialog.tsx
@@ -1,7 +1,9 @@
 import { Box, Heading } from "@chakra-ui/react";
 
-export const ErrorDialog = ({ message }: { message?: string }) => (
-            <Box bg="white" color="blue.800" borderRadius="0.375rem" padding="2.375rem">
-                <Heading>{'Unexpected Error :('}</Heading>
-            </Box>
-)
+export const ErrorDialog = ({ message }: { message?: string }) => {
+    return (
+        <Box bg="white" color="blue.800" borderRadius="0.375rem" padding="2.375rem">
+            <Heading>{message ? message.substring(0, 50) + "..." : 'Unexpected Error :('}</Heading>
+        </Box>
+    )
+}
